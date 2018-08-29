30d1b49df4219cea0b9c99ef464cbd9797f0e54f
ronkeadun <ronkeadun1@yahoo.com>
2018-08-29 04:50:01 +0100

    modified database configuration file

--- server/db/connection.js
+++ server/db/connection.js
@@ -14,7 +14,7 @@ if(process.env.NODE_ENV === 'test') {
         port: process.env.DB_PORT_TEST
     };
 }else if(process.env.NODE_ENV === 'production'){
-    config = process.env.DATABASE_URL;
+    config = {connectionString: process.env.DATABASE_URL};
 }else {
     config = {
         user: process.env.DB_USER,
