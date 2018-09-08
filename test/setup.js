7b0d7975a420b8125c6aa54ad79b94e81a6dc9ca
ronkeadun <ronkeadun1@yahoo.com>
2018-09-08 01:50:49 +0100

    modified travis.yml file

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'test'
 
-process.env.DB_USER_TEST='ronke'
+process.env.DB_USER_TEST='postgres'
 process.env.DB_HOST_TEST='localhost'
 process.env.DB_NAME_TEST='ridesDB'
 process.env.DB_PASSWORD_TEST=3377
